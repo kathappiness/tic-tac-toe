f7cd51c7a93ff0dcf636af069541006131f941ea
kathappiness <kathappiness20@gmail.com>
2025-03-18 21:03:50 +0200

    gameboard update

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -7,13 +7,21 @@ const initialBoard = [
 ]
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialBoard);
+    function handleClick(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        });
+    }
     return (
         <ol id="game-board">
-            {initialBoard.map((row, rowIndex) =>
+            {gameBoard.map((row, rowIndex) =>
                 <li key={rowIndex}>
                     <ol>
-                        {row.map((col, colIndex) =>
-                            <li key={colIndex}>{col}</li>
+                        {row.map((playerSymbol, colIndex) =>
+                            <li key={colIndex}><button onClick={() => handleClick(rowIndex, colIndex)}>{playerSymbol}</button></li>
                         )}
                     </ol>
                 </li>
