c1c644fab0d9b8834cd5d5c8cd4a57ded9c11e5e
kathappiness <kathappiness20@gmail.com>
2025-03-19 09:57:37 +0200

    square changes

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -6,14 +6,15 @@ const initialBoard = [
     [null, null, null],
 ]
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
     const [gameBoard, setGameBoard] = useState(initialBoard);
     function handleClick(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
             const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = 'X';
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
         });
+        onSelectSquare();
     }
     return (
         <ol id="game-board">
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 
-export default function Player({initialName, symbol}) {
+export default function Player({initialName, symbol, isActive}) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setEditing] = useState(false);
 
@@ -16,7 +16,7 @@ export default function Player({initialName, symbol}) {
         editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
     }
     return (
-        <li>
+        <li className={isActive ? 'active' : ''}>
         <span className="player">
             {editablePlayerName}
           <span className="player-symbol">{symbol}</span>
